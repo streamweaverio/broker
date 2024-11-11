82caf0c981afac9b9d60ff1d20e34cf106b65bce
Patrick Foh <patrickfoh@gmail.com>
2024-11-11 10:54:13 +0000

    feat(storage): Add directory initialization in LocalFilesystemDriver

--- internal/storage/local_filesystem_driver.go
+++ internal/storage/local_filesystem_driver.go
@@ -1,7 +1,33 @@
 package storage
 
-type LocalFilesystemDriver struct{}
+import (
+	"fmt"
+	"os"
+)
 
-func NewLocalFilesystemDriver() (*LocalFilesystemDriver, error) {
-	return &LocalFilesystemDriver{}, nil
+type LocalFilesystemDriver struct {
+	Directory string
+}
+
+func InitDirectory(dir string) error {
+	// Check if the directory exists, if not create it
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			return fmt.Errorf("error creating directory: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func NewLocalFilesystemDriver(directory string) (*LocalFilesystemDriver, error) {
+	err := InitDirectory(directory)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LocalFilesystemDriver{
+		Directory: directory,
+	}, nil
 }
