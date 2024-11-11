57b5a00d9b28547cbc40da031dbabe16ee92a6dc
Patrick Foh <patrickfoh@gmail.com>
2024-11-11 10:54:24 +0000

    refactor(storage): Update NewStorageProviderDriver function

--- internal/storage/provider.go
+++ internal/storage/provider.go
@@ -1,13 +1,17 @@
 package storage
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/streamweaverio/broker/internal/config"
+)
 
 type StorageProviderDriver interface{}
 
-func NewStorageProviderDriver(name string) (StorageProviderDriver, error) {
+func NewStorageProviderDriver(name string, cfg *config.StorageConfig) (StorageProviderDriver, error) {
 	switch name {
 	case "local":
-		return NewLocalFilesystemDriver()
+		return NewLocalFilesystemDriver(cfg.Local.Directory)
 	case "s3":
 		return NewS3StorageDriver()
 	default:
