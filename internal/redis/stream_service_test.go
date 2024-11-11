560b0646f9987f93c4df8178aeb94e441ba3db51
Patrick Foh <patrickfoh@gmail.com>
2024-11-11 10:32:44 +0000

    Refactor setupRedisStreamService to include metadataService

--- internal/redis/stream_service_test.go
+++ internal/redis/stream_service_test.go
@@ -2,71 +2,56 @@ package redis
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	rdb "github.com/redis/go-redis/v9"
 	"github.com/streamweaverio/broker/internal/config"
 	"github.com/streamweaverio/broker/internal/testutils"
-	"github.com/streamweaverio/broker/pkg/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 // Helper function for setting up the service and mock client
-func setupRedisStreamService() (*RedisStreamService, *MockRedisClient) {
+func setupRedisStreamService() (*RedisStreamService, *MockRedisClient, *StreamMetadataServiceMock) {
 	client := &MockRedisClient{}
 	logger := testutils.NewMockLogger()
-	opts := &RedisStreamServiceOptions{
-		GlobalRetentionOptions: &config.RetentionConfig{
-			Policy:  "size",
-			MaxSize: 10000,
-			MaxAge:  "1h",
-		},
+	metadataService := NewStreamMetadataServiceMock()
+	retentionOptions := &config.RetentionConfig{
+		Policy:  "size",
+		MaxSize: 10000,
+		MaxAge:  "1h",
 	}
-	service := NewRedisStreamService(context.Background(), client, logger, opts)
-	return service, client
-}
 
-func MetadataKeyMatcher(streamName string) func(interface{}) bool {
-	expectedKey := fmt.Sprintf("%s%s", STREAM_META_DATA_PREFIX, utils.HashString(streamName))
-	return func(value interface{}) bool {
-		key, ok := value.(string)
-		return ok && key == expectedKey
-	}
+	service := NewRedisStreamService(&RedisStreamServiceOptions{
+		Ctx:                    context.Background(),
+		MetadataService:        metadataService,
+		RedisClient:            client,
+		GlobalRetentionOptions: retentionOptions,
+	}, logger)
+
+	return service, client, metadataService
 }
 
 func TestRedisStreamService_CreateStream(t *testing.T) {
 	t.Run("CreateStream with valid parameters and size retention policy", func(t *testing.T) {
-		service, client := setupRedisStreamService()
+		service, client, metadataService := setupRedisStreamService()
 		params := &CreateStreamParameters{
 			Name:            "test-stream",
 			MaxSize:         10000,
 			RetentionPolicy: "size",
 		}
 
-		// Mock HGetAll to return nil, simulating no existing metadata
-		client.On("HGetAll", mock.Anything, mock.MatchedBy(MetadataKeyMatcher(params.Name))).Return(rdb.NewMapStringStringResult(nil, nil))
+		metadataService.On("WriteStreamMetadata", mock.MatchedBy(func(value interface{}) bool {
+			streamMetadata, ok := value.(*StreamMetadata)
+			return ok &&
+				streamMetadata.Name == params.Name &&
+				streamMetadata.RetentionPolicy == params.RetentionPolicy &&
+				streamMetadata.MaxSize == params.MaxSize
+		})).Return(nil)
+		metadataService.On("AddToRetentionBucket", params.Name, params.RetentionPolicy).Return(nil)
 		client.On("XAdd", mock.Anything, mock.MatchedBy(func(args *rdb.XAddArgs) bool {
 			return args.Stream == params.Name
 		})).Return(&rdb.StringCmd{})
-
-		client.On("HSet", mock.Anything, mock.MatchedBy(MetadataKeyMatcher(params.Name)), mock.MatchedBy(func(values []interface{}) bool {
-			if len(values) != 1 {
-				return false
-			}
-			fields, ok := values[0].(map[string]interface{})
-			return ok &&
-				fields["name"] == "test-stream" &&
-				fields["retention_policy"] == "size" &&
-				fields["max_size"] == "10000" &&
-				fields["created_at"] != ""
-		})).Return(&rdb.IntCmd{})
-
-		client.On("SAdd",
-			mock.Anything,
-			STREAM_RETENTION_POLICY_SIZE_BUCKET_KEY,
-			[]interface{}{utils.HashString(params.Name)}).Return(&rdb.IntCmd{})
 
 		client.On("XDel", mock.Anything, "test-stream", mock.Anything).Return(&rdb.IntCmd{})
 
