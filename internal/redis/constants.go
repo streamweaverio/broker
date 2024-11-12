003c0eb0cd85db8d522e4a304739d251ed8b1316
Patrick Foh <patrickfoh@gmail.com>
2024-11-12 14:24:08 +0000

    refactor(redis): Update constants for sharding support

--- internal/redis/constants.go
+++ internal/redis/constants.go
@@ -1,4 +1,8 @@
 package redis
 
-const STREAM_META_DATA_PREFIX = "streams:"
-const STREAM_RETENTION_BUCKET_KEY = "stream_retention_bucket"
+// The curly braces are used to force keys with simiar tags to go the same cluster slot, which is useful for sharding.
+const STREAM_META_DATA_PREFIX = "{streamweaver_stream_metadata}:"
+const STREAM_CLEANUP_BUCKET_DELETE = "stream_cleanup_bucket:delete"
+const STREAM_CLEANUP_BUCKET_ARCHIVE = "stream_cleanup_bucket:archive"
+const STREAM_CLEANUP_BUCKET_DELETE_ARCHIVE = "stream_cleanup_bucket:delete_archive"
+const STREAM_REGISTRY_KEY = "stream_registry"
