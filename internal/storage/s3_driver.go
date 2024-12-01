4281363e4be3bdeeb59b4e32cee68777efb54a55
Patrick Foh <patrickfoh@gmail.com>
2024-12-01 00:07:36 +0000

    feat(storage): Add write method to S3StorageDriver

--- internal/storage/s3_driver.go
+++ internal/storage/s3_driver.go
@@ -1,7 +1,13 @@
 package storage
 
+import "github.com/redis/go-redis/v9"
+
 type S3StorageDriver struct{}
 
 func NewS3StorageDriver() (*S3StorageDriver, error) {
 	return &S3StorageDriver{}, nil
+}
+
+func (s *S3StorageDriver) Write(streamName string, messages []redis.XMessage, meta *ArchiveMetadata) error {
+	return nil
 }
