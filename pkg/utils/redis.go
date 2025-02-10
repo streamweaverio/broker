324a673b81463d9e388280bb6bd9bed6f455f828
Patrick Foh <patrickfoh@gmail.com>
2025-02-10 09:31:08 +0000

    feat(utils): Add Redis stream message serialization and ID timestamp extraction

--- pkg/utils/redis.go
+++ pkg/utils/redis.go
@@ -1,11 +1,12 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 )
 
-// calculates the minimum ID for a Redis stream
+// Calculates the minimum ID for a Redis stream
 func CalculateRedisStreamMinID(retentionTimeInMs int64) (string, error) {
 	maxAgeInSeconds := retentionTimeInMs / 1000
 
@@ -14,4 +15,24 @@ func CalculateRedisStreamMinID(retentionTimeInMs int64) (string, error) {
 	redisTimestamp := fmt.Sprintf("%d-0", cutoffTimestamp)
 
 	return redisTimestamp, nil
+}
+
+// Get the timestamp from a Redis stream message ID
+func GetTimestampFromStreamMessageID(id string) (int64, error) {
+	var timestamp int64
+	_, err := fmt.Sscanf(id, "%d-0", &timestamp)
+	if err != nil {
+		return 0, err
+	}
+
+	return timestamp, nil
+}
+
+func SerializeStreamMessageValues(values map[string]interface{}) string {
+	bytes, err := json.Marshal(values)
+	if err != nil {
+		return "{}"
+	}
+
+	return string(bytes)
 }
