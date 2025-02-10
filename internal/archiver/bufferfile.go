2b9084ab2fa4f0e19d7a1260eb32a3a47f904c22
Patrick Foh <patrickfoh@gmail.com>
2025-02-10 18:46:23 +0000

    refactor(archiver): Use ReadBuffer in BufferFile for reads and seeks

--- internal/archiver/bufferfile.go
+++ internal/archiver/bufferfile.go
@@ -2,48 +2,54 @@ package archiver
 
 import (
 	"bytes"
-	"fmt"
-	"io"
 
 	"github.com/xitongsys/parquet-go/source"
 )
 
-// BufferFile is an implementation of the ParquetFile interface that writes to an in-memorry buffer
+// BufferFile is an implementation of the ParquetFile interface that writes to an in-memory buffer
 type BufferFile struct {
-	Buffer *bytes.Buffer
-	Offset int64
+	Buffer     *bytes.Buffer
+	ReadBuffer *bytes.Reader
+	Offset     int64
 }
 
 func NewBufferFile(buffer *bytes.Buffer, offset int64) *BufferFile {
 	return &BufferFile{
-		Buffer: buffer,
-		Offset: offset,
+		Buffer:     buffer,
+		ReadBuffer: bytes.NewReader(buffer.Bytes()),
+		Offset:     offset,
 	}
 }
 
 func (f *BufferFile) Write(p []byte) (int, error) {
 	n, err := f.Buffer.Write(p)
+	if err == nil {
+		f.ReadBuffer = bytes.NewReader(f.Buffer.Bytes())
+	}
 	f.Offset += int64(n)
 	return n, err
 }
 
 func (f *BufferFile) Read(p []byte) (int, error) {
-	n, err := f.Buffer.Read(p)
+	n, err := f.ReadBuffer.Read(p)
 	f.Offset += int64(n)
 	return n, err
 }
 
 func (f *BufferFile) Open(name string) (source.ParquetFile, error) {
 	return &BufferFile{
-		Buffer: bytes.NewBuffer(f.Buffer.Bytes()),
-		Offset: 0,
+		Buffer:     bytes.NewBuffer(f.Buffer.Bytes()),
+		ReadBuffer: bytes.NewReader(f.Buffer.Bytes()),
+		Offset:     0,
 	}, nil
 }
 
 func (f *BufferFile) Create(name string) (source.ParquetFile, error) {
+	buf := new(bytes.Buffer)
 	return &BufferFile{
-		Buffer: new(bytes.Buffer),
-		Offset: 0,
+		Buffer:     buf,
+		ReadBuffer: bytes.NewReader(buf.Bytes()),
+		Offset:     0,
 	}, nil
 }
 
@@ -52,23 +58,10 @@ func (f *BufferFile) Close() error {
 }
 
 func (f *BufferFile) Seek(offset int64, whence int) (int64, error) {
-	var abs int64
-	switch whence {
-	case io.SeekStart:
-		abs = offset
-	case io.SeekCurrent:
-		abs = f.Offset + offset
-	case io.SeekEnd:
-		abs = int64(f.Buffer.Len()) + offset
-	default:
-		return 0, fmt.Errorf("invalid whence value: %d", whence)
+	abs, err := f.ReadBuffer.Seek(offset, whence)
+	if err != nil {
+		return 0, err
 	}
-
-	if abs < 0 {
-		return 0, fmt.Errorf(("negative position: %d"), abs)
-	}
-
-	f.Buffer = bytes.NewBuffer(f.Buffer.Bytes()[abs:])
 	f.Offset = abs
 	return abs, nil
 }
