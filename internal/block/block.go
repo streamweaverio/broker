abcd213505dbfc577a99fcff60bdf9246c19fb5c
Patrick Foh <patrickfoh@gmail.com>
2025-02-10 11:36:11 +0000

    feat(block): Add new block structures and metadata

---
+++ internal/block/block.go
@@ -0,0 +1,33 @@
+package block
+
+import "io"
+
+type Block struct {
+	StreamName string
+	BlockID    string
+	Parquet    io.ReadCloser
+	Bloom      io.ReadCloser
+	Meta       []byte
+}
+
+type BlockMetadata struct {
+	// Name of the stream
+	StreamName string `json:"stream_name"`
+	// ID of the block
+	BlockID string `json:"block_id"`
+	// Start timestamp of the block
+	BlockStartTimestamp int64 `json:"block_start"`
+	// End timestamp of the block
+	BlockEndTimestamp int64 `json:"block_end"`
+	// ID of the first message in the block
+	BlockStartId string `json:"block_start_id"`
+	// ID of the last message in the block
+	BlockEndId string `json:"block_end_id"`
+	// Number of messages in the block
+	MessageCount int `json:"message_count"`
+}
+
+type BlockParquet struct {
+	MessageID string `parquet:"name=message_id, type=UTF8, encoding=PLAIN_DICTIONARY"`
+	Data      string `parquet:"name=data, type=BYTE_ARRAY, convertedtype=UTF8, encoding=PLAIN_DICTIONARY"`
+}
